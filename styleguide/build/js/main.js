725b76d41cc03d9a747f5f80f431439700447e3e
Yann Gouffon <yago@yago-art.ch>
2014-04-29 15:28:48 +0200

    add breacrumb start

--- styleguide/build/js/main.js
+++ styleguide/build/js/main.js
@@ -63,7 +63,7 @@
 (function($) {
   $(document).ready(function() {
     // Add wrapper to all styleguide content
-    $("p, h1, h2, h3, h4, h5, h6, table").not(".cortana-header h1, .codeExample p, .codeExample table, .codeExample h1, .codeExample h2, .codeExample h3, .codeExample h4, .codeExample h5, .codeExample h6").wrap('<div class="cortana-content"></div>');
+    $("p, h1, h2, h3, h4, h5, h6, table, ul, ol, dl").not(".cortana-header h1, .codeExample p, .codeExample table, .codeExample h1, .codeExample h2, .codeExample h3, .codeExample h4, .codeExample h5, .codeExample h6, .codeExample ul, .codeExample dl, .codeExample ol").wrap('<div class="cortana-content"></div>');
   });
 }) (jQuery);
 /*global  jQuery */
