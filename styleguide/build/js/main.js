32b7ada38c760d40c9311b194ca23fd9727105bc
Yann Gouffon <yago@yago-art.ch>
2014-04-30 09:31:39 +0200

    jquery no-conflict

--- styleguide/build/js/main.js
+++ styleguide/build/js/main.js
@@ -42,7 +42,7 @@
       }, 800);
     });
   });
-}) (jQuery);
+}) (jQuery_no_conflict);
 /*global  jQuery */
 
 (function($) {
@@ -57,7 +57,7 @@
     });
 
   });
-}) (jQuery);
+}) (jQuery_no_conflict);
 /*global  jQuery */
 
 (function($) {
@@ -65,5 +65,5 @@
     // Add wrapper to all styleguide content
     $("p, h1, h2, h3, h4, h5, h6, table, ul, ol, dl").not(".cortana-header h1, .codeExample p, .codeExample table, .codeExample h1, .codeExample h2, .codeExample h3, .codeExample h4, .codeExample h5, .codeExample h6, .codeExample ul, .codeExample dl, .codeExample ol").wrap('<div class="cortana-content"></div>');
   });
-}) (jQuery);
+}) (jQuery_no_conflict);
 /*global  jQuery */
