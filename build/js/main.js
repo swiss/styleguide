95910a6fa3bb496643ff7d5d85abbf1575831eec
Yann Gouffon <yago@yago-art.ch>
2014-04-30 15:12:26 +0200

    rebuild

--- build/js/main.js
+++ build/js/main.js
@@ -56,3 +56,17 @@
   });
 
 }) (jQuery);
+
+/* ==========================================================
+ * select.js
+ * Scripts handling `select` elements
+ *
+ * Author: Toni Fisler, toni@antistatique.net
+ * Date:   2014-04-30 10:20:33
+ *
+ * Copyright 2014 Federal Chancellery of Switzerland
+ * Licensed under MIT
+ ========================================================== */
+$(document).ready(function(){
+  $('select').chosen({disable_search_threshold: 10});
+});
