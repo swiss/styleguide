4a065a845583867dce3cebd33dfcc829f5461a96
Yann Gouffon <yago@yago-art.ch>
2014-05-06 09:30:51 +0200

    add breadcrumb

--- build/js/main.js
+++ build/js/main.js
@@ -9,35 +9,37 @@
  * Licensed under MIT
  ========================================================== */
 
-(function($, data) {
+if (typeof searchData != "undefined") {
+  (function($, data) {
 
-  var $searchField = $('#search-field');
+    var $searchField = $('#search-field');
 
-  // Init the Bloodhound suggestion engine
-  var bloodhound = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    local: $.map(data, function(state) { return { value: state }; })
-  });
-  bloodhound.initialize();
+    // Init the Bloodhound suggestion engine
+    var bloodhound = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: $.map(data, function(state) { return { value: state }; })
+    });
+    bloodhound.initialize();
 
-  // Init Typeahead on search-fields
-  $searchField.typeahead({
-    hint: true,
-    highlight: true,
-    minLength: 1,
-  },
-  {
-    name: 'search',
-    displayKey: 'value',
-    source: bloodhound.ttAdapter()
-  });
+    // Init Typeahead on search-fields
+    $searchField.typeahead({
+      hint: true,
+      highlight: true,
+      minLength: 1,
+    },
+    {
+      name: 'search',
+      displayKey: 'value',
+      source: bloodhound.ttAdapter()
+    });
 
-  // Insert the icons
-  $('<span class="icon icon--close" onclick="$(\'#search-field\').focus().val(\'\');"></span>').insertAfter($searchField);
-  $('.form-search').append('<span class="icon icon--search"></span>');
+    // Insert the icons
+    $('<span class="icon icon--close" onclick="$(\'#search-field\').focus().val(\'\');"></span>').insertAfter($searchField);
+    $('.form-search').append('<span class="icon icon--search"></span>');
 
-}) (jQuery, searchData);
+  }) (jQuery, searchData);
+}
 /* ==========================================================
  * rich-menu.js
  * Add overlay when openning a rich yamm menu and define open/close events
