981466a064646bc51a631cfa9c99faf8909c5a96
Toni Fisler <mail@tonifisler.ch>
2014-06-04 10:24:46 +0200

    fix massive issue #91

--- styleguide-theme/assets/js/inside-navigation.js
+++ styleguide-theme/assets/js/inside-navigation.js
@@ -2,40 +2,40 @@
   $(document).ready(function () {
     // Init stick inside nav
     if ($(window).width() > 1280) {
-      $("#admin-inside-nav").stick_in_parent()
+      $("#cortana-inside-nav").stick_in_parent()
         .on("sticky_kit:bottom", function () {
           console.log('toggle bottom');
-          $(this).addClass("admin-stick-bottom");
+          $(this).addClass("cortana-stick-bottom");
         })
         .on("sticky_kit:unbottom", function () {
           console.log('toggle bottom');
-          $(this).removeClass("admin-stick-bottom");
+          $(this).removeClass("cortana-stick-bottom");
         });
     }
 
     // Remove stick feature below 1280px
     $(window).resize(function () {
       if ($(window).width() > 1280) {
-        $("#admin-inside-nav").stick_in_parent()
+        $("#cortana-inside-nav").stick_in_parent()
           .on("sticky_kit:bottom", function () {
             console.log('toggle bottom');
-            $(this).addClass("admin-stick-bottom");
+            $(this).addClass("cortana-stick-bottom");
           })
           .on("sticky_kit:unbottom", function () {
             console.log('toggle bottom');
-            $(this).removeClass("admin-stick-bottom");
+            $(this).removeClass("cortana-stick-bottom");
           });
       } else {
-        $("#admin-inside-nav").trigger("sticky_kit:detach");
+        $("#cortana-inside-nav").trigger("sticky_kit:detach");
         console.log('unstick');
       }
     });
 
     // Scroll To position
-    $('#admin-inside-nav').find('a').click(function (event) {
+    $('#cortana-inside-nav').find('a').click(function (event) {
       event.preventDefault();
       $target = $(this).attr('href');
-      $('#admin-inside-nav').find('a').removeClass('active');
+      $('#cortana-inside-nav').find('a').removeClass('active');
       $(this).addClass('active');
       $('html, body').animate({
         scrollTop: $($target).offset().top - 100
