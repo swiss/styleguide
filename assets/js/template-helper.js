6006229cea7cf8f200270032a53051431ed20e29
Toni Fisler <mail@tonifisler.ch>
2014-09-08 10:23:31 +0200

    Update 2014-09-08T08:23:31.682Z

---
+++ assets/js/template-helper.js
@@ -0,0 +1,16 @@
+/*global  $ */
+(function ($) {
+  $(document).ready(function () {
+    $('table').not('.codeExample table').addClass('table').addClass('table-bordered').css('margin-top', '20px');
+
+    var codeIndex = 0;
+    $('.codeExample').each(function () {
+      var $output = $('.exampleOutput'),
+        $markup = $('.codeBlock');
+
+      codeIndex += 1;
+      $(this).find($output).append('<button type="button" class="show-code" data-toggle="collapse" data-target="#codeBlock-' + codeIndex + '">&lt;/&gt;</button>');
+      $(this).find($markup).addClass('collapse').attr('id', 'codeBlock-' + codeIndex);
+    });
+  });
+})(jQuery_no_conflict);
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    notify = require('gulp-notify'),
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    browserSync = require('browser-sync'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    shell = require('gulp-shell'),
+    stylish = require('jshint-stylish');
+
+// SASS compile, autoprefix and minify task
+gulp.task('styles', function() {
+  return gulp.src('./assets/sass/admin-theme.scss')
+    .pipe(sass())
+    .on('error', gutil.beep)
+    .on('error', notify.onError("Error: <%= error.message %>"))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(minifycss())
+    .pipe(rename('admin-theme.min.css'))
+    .pipe(gulp.dest('./theme-build/css'));
+});
+
+// CSS vendors concat and minify
+gulp.task('css_vendors', function() {
+  gulp.src([
+      './vendors/Slidebars/distribution/0.9.4/slidebars.css'
+    ])
+    .pipe(concat('vendors.min.css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./theme-build/css'));
+});
+
+// JS task
+gulp.task('scripts', function() {
+  gulp.src('./assets/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./theme-build/js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./theme-build/js'));
+});
+
+// JS vendors concat and minify
+gulp.task('js_vendors', function() {
+  gulp.src([
+      './vendors/jquery/jquery.js',
+      './vendors/Slidebars/distribution/0.9.4/slidebars.min.js',
+      './vendors/sticky-kit/jquery.sticky-kit.js',
+      './vendors/hogan/web/builds/2.0.0/hogan-2.0.0.js',
+      './vendors/typeahead.js/dist/typeahead.js'
+    ])
+    .pipe(concat('vendors.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./theme-build/js'));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init(['../docs/build/css/*.css', '../docs/*.html', '../docs/build/js/*.js'], {
+      proxy: 'localhost',
+      open: false
+    });
+});
+
+gulp.task('default', ['js_vendors', 'css_vendors', 'styles', 'scripts']);
+
+gulp.task('watch', function() {
+  gulp.watch('./assets/sass/*.scss', ['styles']);
+  gulp.watch('./assets/js/*.js', ['scripts']);
+});
