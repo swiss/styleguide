e2dab463a3606132aca9d56590b0485e99dd93f8
Toni Fisler <mail@tonifisler.ch>
2014-09-09 11:07:04 +0200

    remove fade and add hover stop for focus element rel #228

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -9,14 +9,25 @@
   * Licensed under MIT
   ========================================================== */
 
- $(function() {
-  // Autoplay for tabs-focus elements
-  var tabCarousel = setInterval(function() {
-      var tabs = $('.nav-tabs-focus.nav-tabs > li'),
-          active = tabs.filter('.active'),
-          next = active.next('li'),
-          toClick = next.length ? next.find('a') : tabs.eq(0).find('a');
+(function($) {
 
-      toClick.trigger('click');
-  }, 3000);
- }) (jQuery);
+  // Autoplay for tabs-focus elements
+  var interval = 3000;
+  var tabCarousel = setInterval(nextSlide, interval);
+
+  $('.tab-content.tab-focus, .nav-tabs.nav-tabs-focus').hover(function(e){
+      clearInterval(tabCarousel);
+  }, function(e){
+      tabCarousel = setInterval( nextSlide, interval);
+  });
+
+  function nextSlide() {
+    var tabs = $('.nav-tabs-focus.nav-tabs > li'),
+        active = tabs.filter('.active'),
+        next = active.next('li'),
+        toClick = next.length ? next.find('a') : tabs.eq(0).find('a');
+
+    toClick.trigger('click');
+  }
+
+}) (jQuery);
