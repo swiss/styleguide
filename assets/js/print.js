adb0c663a91f0f38b2b2ec5d71aa759f97fc5c4c
Yann Gouffon <yago@yago-art.ch>
2015-02-03 14:27:04 +0100

    #353 add print preview mode

--- assets/js/print.js
+++ assets/js/print.js
@@ -19,14 +19,77 @@
   $.printPreview = {
 
     printPreview: function() {
-      var $body = $('body');
+      var $body = $('body'),
+          $container = $('.container-main'),
+          footnoteLinks = "",
+          linksIndex = 0;
 
-      $body.find('.nav-mobile, .drilldown, .nav-main, .header-separator, .nav-service, .nav-lang, .form-search, .yamm--select, header > div:first-child, footer, .alert, .icon--print, .social-sharing, form, .nav-process, .carousel-indicators, .carousel-control, .col-md-4, .breadcrumb').remove();
+      $body.find('.nav-mobile, .drilldown, .nav-main, .header-separator, .nav-service, .nav-lang, .form-search, .yamm--select, header > div:first-child, footer, .alert, .icon--print, .social-sharing, form, .nav-process, .carousel-indicators, .carousel-control, .breadcrumb').remove();
       $body.addClass('print-preview');
+
+      $container.prepend('<div class="row" id="print-settings">'+
+        '<div class="col-sm-12">'+
+          '<nav class="pagination-container clearfix">'+
+            '<span class="pull-left checkbox checkbox--left">'+
+              '<input type="checkbox" id="footnote-links">'+
+              '<label for="footnote-links">Links as footnotes</label>'+
+            '</span>'+
+            '<ul class="pull-right pagination">'+
+              '<li>'+
+                '<button id="print-button" title="print" class="btn"><span class="icon icon--print"></span></button>'+
+                '&nbsp;&nbsp;'+
+                '<button id="close-button" title="close" class="btn btn-secondary"><span class="icon icon--close"></span></button>'+
+              '</li>'+
+            '</ul>'+
+          '</nav>'+
+        '</div>'+
+      '</div>');
+
+      $('#print-button').click(function () {
+        $.printPreview.printProcess();
+      });
+
+      $('#close-button').click(function () {
+        $.printPreview.printClose();
+      });
+
+
+      $('a').not('.access-keys a').each(function () {
+        var target = $(this).attr('href');
+        target = String(target);
+
+        if (target != "undefined" && target.indexOf("#") >= 0) {
+          linksIndex ++;
+          footnoteLinks += '<li>'+target+'</li>';
+          $('<sup class="link-ref">('+linksIndex+')</sup>').insertAfter(this);
+        }
+      });
+
+
+      $('#footnote-links').change(function(){
+        if (this.checked) {
+          $container.append('<div id="footnote-links-wrapper" class="row footnote-links-wrapper">'+
+            '<div class="col-sm-12">'+
+            '<h3>Page Links</h3><hr>'+
+            '<ol>'+
+              footnoteLinks+
+            '</ol>'+
+            '</div>'+
+          '</div>');
+          $body.addClass('print-footnotes');
+        } else {
+          $('#footnote-links-wrapper').remove();
+          $body.removeClass('print-footnotes');
+        }
+      });
     },
 
     printProcess: function() {
-      console.log('hello');
+      window.print();
+    },
+
+    printClose: function() {
+      window.location.reload();
     }
 
   };
@@ -39,5 +102,8 @@
           return false;
       }
   });
+
+  // To test print preview mode
+  //$.printPreview.printPreview();
 
  }) (jQuery);
