f7d468659cfab47f1c5e815e3a3a0d35b56c7cd3
René Giller BIT <rene.giller@bit.admin.ch>
2017-11-28 15:45:36 +0100

    chore: update build folder

--- build/js/main.js
+++ build/js/main.js
@@ -483,6 +483,112 @@ $.printPreview = {
 
 }) (jQuery);
 
+(function($) {
+  'use strict';
+
+  var datasets = [];
+  $('.dropdown.yamm-fw').each(function() {
+    var title = $('.dropdown-toggle', $(this)).html();
+    var links = $('.dropdown-menu li a', $(this));
+    var suggestions = [];
+    links.each(function() {
+        suggestions.push({
+            title: $(this).html(),
+            link: $(this).attr('href')
+        });
+    });
+    if (!suggestions.length) {
+      return;
+    }
+    var engine = new Bloodhound({
+      initialize: true,
+      local: suggestions,
+      identify: function(obj) {
+        return obj.link;
+      },
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace
+    });
+    datasets.push({
+      display: 'title',
+      source: engine,
+      templates: {
+        empty: function() {
+          return [
+            '<li><h3>',
+              title,
+            '</h3></li>',
+            '<li>',
+              window.translations['global-search']['nothing-found'],
+            '</li>',
+          ].join('');
+        },
+        header: function() {
+          return [
+            '<li><h3>',
+              title,
+            '</h3></li>'
+          ].join('');
+        },
+        dataset: '<ul><ul>',
+        suggestion: function (data) {
+          return '<li><a href="' + data.link + '">' + data.title + '</a></li>';
+        }
+      }
+    });
+  });
+
+  function initTypeahead(element) {
+    $('.search-input', element).typeahead({
+      highlight: true,
+      menu: $('.search-results .search-results-list', element),
+      classNames: {
+        suggestion: '',
+        cursor: 'active'
+      }
+    }, datasets)
+    .on('typeahead:selected', function (event, selection) {
+  		event.preventDefault();
+      $(this).typeahead('val', '')
+        .closest('.global-search').removeClass('has-input');
+  		window.location.replace(selection.link);
+  	})
+    .on('typeahead:open', function() {
+      $(this).closest('.global-search').addClass('focused');
+      console.log($(this).typeahead('val'));
+    })
+    .on('typeahead:close', function () {
+      $(this).closest('.global-search').removeClass('focused');
+    })
+    .on('keyup', function (event) {
+      if (event.keyCode === 27) { // ESC
+        $(this).closest('form').trigger('reset');
+      } else if ($(this).typeahead('val')) {
+          $(this).closest('.global-search').addClass('has-input');
+      } else {
+        $(this).closest('.global-search').removeClass('has-input');
+  		}
+  	});
+
+    $('form', element)
+      .on('submit', function() {
+        return false;
+      })
+      .on('reset', function() {
+        $('.search-input', this).blur().typeahead('val', '');
+        $(this).closest('.global-search').removeClass('has-input');
+      });
+
+    $('.search-reset', element).on('click', function() {
+      $(this).closest('form').trigger('reset');
+    });
+  }
+
+  initTypeahead($('.global-search-standard'));
+  initTypeahead($('.global-search-mobile'));
+
+})(jQuery);
+
 /* ==========================================================
  * select.js
  * Scripts handling `select` elements
@@ -655,6 +761,56 @@ $.printPreview = {
 
 }) (jQuery);
 
+(function($) {
+  'use strict';
+
+  $('a.social-sharing-facebook').click(function() {
+    window.open(
+      'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(location.href),
+      'facebook-share-dialog',
+      'width=626,height=436'
+    );
+    return false;
+  });
+
+  $('a.social-sharing-twitter').click(function() {
+    window.open(
+      'http://twitter.com/share?url=' + encodeURIComponent(location.href),
+      'twitter-share-dialog',
+      'width=626,height=436'
+    );
+    return false;
+  });
+
+  $('a.social-sharing-google').click(function() {
+    window.open(
+      'https://plus.google.com/share?url=' + encodeURIComponent(location.href),
+      'google-share-dialog',
+      'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600'
+    );
+    return false;
+  });
+
+  $('a.social-sharing-xing').click(function() {
+    window.open(
+      'https://www.xing.com/spi/shares/new?url=' + encodeURIComponent(location.href),
+      'xing-share-dialog',
+      'width=626,height=436'
+    );
+    return false;
+  });
+
+  $('a.social-sharing-linkedin').click(function() {
+    window.open(
+      'https://www.linkedin.com/cws/share?url=' + encodeURIComponent(location.href),
+      'linkedin-share-dialog',
+      'width=626,height=436'
+    );
+    return false;
+  });
+
+})(jQuery);
+
 /* ==========================================================
  * subnavigation.js
  * Sub-navigation scripts, handles mainly how the nav-page-list behaves on small
@@ -720,7 +876,7 @@ function subNavInit($) {
 (function($) {
   'use strict';
 
-  $('.table-sort').tablesorter();
+  $('.table-sort').tablesorter({ sortLocaleCompare : true });
 
 }) (jQuery);
 
@@ -859,3 +1015,110 @@ function subNavInit($) {
   });
 
 }) (jQuery);
+
+(function($){
+  'use strict';
+
+  // Setup twitter widget (see: https://dev.twitter.com/web/javascript/loading)
+  window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+      t = window.twttr || {};
+    if (d.getElementById(id)) {
+      return t;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    t._e = [];
+    t.ready = function(f) {
+      t._e.push(f);
+    };
+    return t;
+  }(document, "script", "twitter-wjss"));
+
+  window.twttr.ready(function (twitter) {
+    $('.mod-twitterstream .twitter-timeline').each(function () {
+      twitter.widgets.createTimeline(
+        {
+          sourceType: 'profile',
+          screenName: $(this).data('profile')
+        },
+        this,
+        {
+          borderColor: '#ccc', // $silver
+          linkColor: '#069', // $cerulean
+          chrome: 'noheader nofooter transparent',
+          tweetLimit: $(this).data('tweet-limit')
+        })
+        .then(function (iframe) {
+          var head = $(iframe).contents().find('head');
+          if (head.length) {
+            head.append('<link type="text/css" rel="stylesheet" href="../css/twitter-inject.css" />');
+
+            /*
+            Injected CSS has an impact on the iframe body's height.
+            So we need to force widget.js to recalculate the iframe height.
+             */
+            $(iframe).css('height', '100%');
+
+            /*
+            As we apply some custom styling which is not intendend by Twitter,
+            let's try to detect any changes by Twitter that may break it.
+            */
+            reportTwitterHtmlChanges($(iframe));
+          }
+        });
+      twitter.widgets.createFollowButton(
+        $(this).data('profile'),
+        $(this).parent().get(0),
+        {
+          showCount: false
+        }
+      );
+    });
+  });
+
+  function reportTwitterHtmlChanges(iframe) {
+    reportMissingTwitterHmlElement(iframe, '.TweetAuthor-name');
+    reportMissingTwitterHmlElement(iframe, '.TweetAuthor-link');
+    reportMissingTwitterHmlElement(iframe, '.TweetAuthor-avatar');
+    reportMissingTwitterHmlElement(iframe, '.SandboxRoot.env-bp-min .TweetAuthor-avatar');
+    reportMissingTwitterHmlElement(iframe, '.TweetAuthor-avatar .Avatar');
+    reportMissingTwitterHmlElement(iframe, '.SandboxRoot.env-bp-min .TweetAuthor-avatar .Avatar');
+    reportMissingTwitterHmlElement(iframe, '.timeline-Tweet-author');
+    reportMissingTwitterHmlElement(iframe, '.TweetAuthor-name.Identity-name');
+    reportMissingTwitterHmlElement(iframe, '.TweetAuthor-screenName.Identity-screenName');
+    reportMissingTwitterHmlElement(iframe, '.timeline-Tweet-text');
+    reportMissingTwitterHmlElement(iframe, '.SandboxRoot.env-bp-min .timeline-Tweet-text');
+    reportMissingTwitterHmlElement(iframe, '.timeline-Tweet-action.timeline-ShareMenu');
+    reportMissingTwitterHmlElement(iframe, '.timeline-Tweet-brand');
+    reportMissingTwitterHmlElement(iframe, '.TweetAuthor-verifiedBadge');
+    reportMissingTwitterHmlElement(iframe, '.timeline-Tweet-metadata *');
+    reportMissingTwitterHmlElement(iframe, '.timeline-Tweet-retweetCredit', true);
+    reportMissingTwitterHmlElement(iframe, '.Icon.Icon--retweetBadge', true);
+    reportMissingTwitterHmlElement(iframe, '.Icon.Icon--heart.TweetAction-icon.Icon--heartEdge');
+    reportMissingTwitterHmlElement(iframe, '.TweetAction .Icon.Icon--heart.TweetAction-icon.Icon--heartEdge');
+    reportMissingTwitterHmlElement(iframe, '.timeline-Body');
+    reportMissingTwitterHmlElement(iframe, '.timeline-ShowMoreButton');
+  }
+
+  function reportMissingTwitterHmlElement(iframe, selector, isOptional) {
+    if (0 < $(iframe).contents().find(selector).length) {
+      return;
+    }
+    var message = [
+      'mod-twitterstream #',
+      $(iframe).attr('id'),
+      ': The following ',
+      isOptional ? ' optional ' : '',
+      'elements could not be found inside Twitter widget HTML: "',
+      selector,
+      '". This may be caused by a Twitter widget HTML change. ',
+      'In this case, Twitter stream may not be CI/CD compliant.'
+    ].join('');
+
+    isOptional ? console.log(message) : console.warn(message);
+  }
+})(jQuery);
